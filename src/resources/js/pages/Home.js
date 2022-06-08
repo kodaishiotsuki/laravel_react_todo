e831dd8ce2e1caad498bac483ca698054e81d4a6
kodaishiotsuki <0711dr5@gmail.com>
2022-06-08 19:01:27 +0900

    material UI

---
+++ src/resources/js/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+export default function Home() {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                <div>test</div>
+            </Grid>
+        </Grid>
+    );
+}
