d6d5cb8619bb2bce64215abc93c72b9cd7cb7abb
kodaishiotsuki <0711dr5@gmail.com>
2022-06-08 17:14:56 +0900

    deleteオーバーライド

--- src/app/Models/ToDo.php
+++ src/app/Models/ToDo.php
@@ -13,4 +13,15 @@ class ToDo extends Model
     {
         return $this->hasMany(ToDoDetail::class);
     }
+
+    public function delete()
+    {
+        //オーバーライド//
+
+        //関連するToDoDetailsのレコードを削除する
+        $this->toDoDetails()->delete();
+
+        //ToDoのレコードを削除する
+        return parent::delete();
+    }
 }
